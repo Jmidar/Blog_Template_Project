e72e55fc5e57aff4f7b12c2c6f68b8ac3bb3d9b1
Arnab Das <arnab.das682@gmail.com>
2018-03-03 15:48:11 +0600

    Post delete Option

--- admin/editpost.php
+++ admin/editpost.php
@@ -9,146 +9,178 @@
         //var_dump($postid);
     }
 ?>
-        <div class="grid_10">
-		
-            <div class="box round first grid">
-                <h2>Edit Post</h2>
-             <?php
 
-                if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 
-                $title = mysqli_real_escape_string($db->link, $_POST['title']);
-                $cat = mysqli_real_escape_string($db->link, $_POST['cat']);
-                $body = mysqli_real_escape_string($db->link, $_POST['body']);
-                $tags = mysqli_real_escape_string($db->link, $_POST['tags']);
-                $author = mysqli_real_escape_string($db->link, $_POST['author']);
+  <div class="grid_10">
+<?php
+$query = "SELECT * FROM tbl_post WHERE id='$postid' ORDER BY id desc";
+$getpost = $db->select($query);
+  if ($getpost){
+    while ($postresult = $getpost->fetch_assoc()) {
+      
+    
+?>
 
-                 $permited  = array('jpg', 'jpeg', 'png', 'gif');
-                 $file_name = $_FILES['image']['name'];
-                 $file_size = $_FILES['image']['size'];
-                 $file_temp = $_FILES['image']['tmp_name'];
+<div class="box round first grid">
+    <h2>Update Post</h2>
+ <?php
 
-                 $div = explode('.', $file_name);
-                 $file_ext = strtolower(end($div));
-                 $unique_image = substr(md5(time()), 0, 10).'.'.$file_ext;
-                 $uploaded_image = "Upload/".$unique_image;
+    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 
-                 if ($title == "" || $cat == "" || $body == "" || $tags == "" || $author == "" || $file_name == ""){
+    $title = mysqli_real_escape_string($db->link, $_POST['title']);
+    $cat = mysqli_real_escape_string($db->link, $_POST['cat']);
+    $body = mysqli_real_escape_string($db->link, $_POST['body']);
+    $tags = mysqli_real_escape_string($db->link, $_POST['tags']);
+    $author = mysqli_real_escape_string($db->link, $_POST['author']);
 
-                    echo "<span style='color: red;font-size: 18px;'>Field must not be Empty...!!!</span>";
-                 }elseif ($file_size >1048567) {
-                     echo "<span class='error'>Image Size should be less then 1MB!
-                             </span>";
-                  } elseif (in_array($file_ext, $permited) === false) {
-                     echo "<span class='error'>You can upload only:-"
-                        .implode(', ', $permited)."</span>";
-                 } else{
-                      move_uploaded_file($file_temp, $uploaded_image);
-                      $query = "INSERT INTO tbl_post(cat, title, body, image, author, tags) VALUES('$cat', '$title', '$body', '$uploaded_image', '$author', '$tags')";
-                      //var_dump($query);
-                      $inserted_rows = $db->insert($query);
-                      if ($inserted_rows) {
-                      echo "<span class='success'>Post Inserted Successfully.
-                      </span>";
-                      }else {
-                           echo "<span class='error'>Post Not Inserted !</span>";
-                           }
-                    } 
-                }
-             ?>
-                <div class="block">
-       <?php
-         $query = "SELECT * FROM tbl_post where id=$postid ORDER BY id desc";
-         //var_dump($query);
-         $getpost = $db->select($query);
-              //var_dump($getpost);   
-         while ($postresult = $getpost->fetch_assoc()) {
-                  //var_dump($postresult);
-       ?>               
-                 <form action="" method="post" enctype="multipart/form-data">
-                    <table class="form">
-                       
-                        <tr>
-                            <td>
-                                <label>Title</label>
-                            </td>
-                            <td>
-                                <input type="text" value="<?php echo $postresult['title']; ?>" class="medium" />
-                            </td>
-                        </tr>
-                     
-                        <tr>
-                            <td>
-                                <label>Category</label>
-                            </td>
-                            <td>
-                                <select id="select" name="cat">
-                                    <option>Select Category</option>
-                              <?php
-                                $query = "SELECT * FROM tbl_category";
-                                $category = $db->select($query);
-                                 if ($category){
-                                    while ($result = $category->fetch_assoc()) {
-                                         
-                              ?>
-                                    <option 
-                                  <?php if($postresult['cat'] == $result['id']){ ?>
-                                      Selected = "Selected"
-                                  <?php } ?> value="<?php echo $result['id']; ?>"><?php echo $result['name']; ?></option>
-                              <?php  } } ?>
-                                </select>
-                            </td>
-                        </tr>
-                   
-                        <tr>
-                            <td>
-                                <label>Upload Image</label>
-                            </td>
-                            <td>
-                                <img src="<?php echo $postresult['image']; ?>" height="150px" width="200px" /><br>
-                                <input type="file" name="image" />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td style="vertical-align: top; padding-top: 9px;">
-                                <label>Content</label>
-                            </td>
-                            <td>
-                                <textarea class="tinymce" name="body" >
-                                  <?php echo $postresult['body']; ?>
-                                </textarea>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label>Tags</label>
-                            </td>
-                            <td>
-                                <input type="text" name="tags" 
-                                value="<?php echo $postresult['tags']; ?>" class="medium" />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label>Author</label>
-                            </td>
-                            <td>
-                                <input type="text" name="author" 
-                                value="<?php echo $postresult['author']; ?>" class="medium" />
-                            </td>
-                        </tr>
-					             	<tr>
-                            <td></td>
-                            <td>
-                                <input type="submit" name="submit" Value="Save" />
-                            </td>
-                        </tr>
-                    </table>
-                    </form>
-        <?php } ?>
-                </div>
-            </div>
-        </div>
+     $permited  = array('jpg', 'jpeg', 'png', 'gif');
+     $file_name = $_FILES['image']['name'];
+     $file_size = $_FILES['image']['size'];
+     $file_temp = $_FILES['image']['tmp_name'];
+
+     $div = explode('.', $file_name);
+     $file_ext = strtolower(end($div));
+     $unique_image = substr(md5(time()), 0, 10).'.'.$file_ext;
+     $uploaded_image = "Upload/".$unique_image;
+
+   if ($title == "" || $cat == "" || $body == "" || $tags == "" || $author == "" ){
+
+      echo "<span style='color: red;font-size: 18px;'>Field must not be Empty...!!!</span>";
+   } else {
+  if (!empty($file_ext)){
+
+  
+     if ($file_size >1048567) {
+         echo "<span class='error'>Image Size should be less then 1MB!
+                 </span>";
+      } elseif (in_array($file_ext, $permited) === false) {
+         echo "<span class='error'>You can upload only:-"
+            .implode(', ', $permited)."</span>";
+     } else{
+          move_uploaded_file($file_temp, $uploaded_image);
+          $query = "UPDATE tbl_post 
+                    set 
+                    cat = '$cat',
+                    title = '$title',
+                    body = '$body',
+                    image = '$uploaded_image',
+                    author = '$author',
+                    tags = '$tags' 
+                    WHERE id = '$postid'";
+          //var_dump($query);
+          $updated_rows = $db->update($query);
+          if ($updated_rows) {
+          echo "<span class='success'>Post data updated Successfully.
+          </span>";
+          }else {
+               echo "<span class='error'>Post data Not updated !</span>";
+               }
+        } 
+    }else {
+       $query = "UPDATE tbl_post 
+                  set 
+                  cat = '$cat',
+                  title = '$title',
+                  body = '$body',
+                  author = '$author',
+                  tags = '$tags' 
+                  WHERE id = '$postid'";
+        //var_dump($query);
+        $updated_rows = $db->update($query);
+        if ($updated_rows) {
+        echo "<span class='success'>Post data updated Successfully.</span>";
+        }else {
+             echo "<span class='error'>Post data Not updated !</span>";
+             }
+      }
+    }
+  }
+ ?>
+          <div class="block">               
+           <form action="" method="post" enctype="multipart/form-data">
+              <table class="form">
+                 
+                  <tr>
+                      <td>
+                          <label>Title</label>
+                      </td>
+                      <td>
+                          <input type="text" name="title" value = "<?php echo $postresult['title']; ?>" class="medium" />
+                      </td>
+                  </tr>
+               
+                  <tr>
+                      <td>
+                          <label>Category</label>
+                      </td>
+                      <td>
+                          <select id="select" name="cat">
+                              <option>Select Category</option>
+                        <?php
+                          $query = "SELECT * FROM tbl_category";
+                          $category = $db->select($query);
+                           if ($category){
+                              while ($result = $category->fetch_assoc()) {
+                                   
+                        ?>
+                              <option 
+                            <?php 
+                              if($postresult['cat']== $result['id']){
+                             ?> selected = "selected';
+                             <?php } ?>
+                               value="<?php echo $result['id'] ?>"><?php echo $result['name']; ?></option>
+                        <?php  } } ?>
+                          </select>
+                      </td>
+                  </tr>
+             
+                  <tr>
+                      <td>
+                          <label>Upload Image</label>
+                      </td>
+                      <td>
+                          <img src="<?php echo $postresult['image']; ?>" height="80px" wight="200px" />
+                          <input type="file" name="image" />
+                      </td>
+                  </tr>
+                  <tr>
+                      <td style="vertical-align: top; padding-top: 9px;">
+                          <label>Content</label>
+                      </td>
+                      <td>
+                          <textarea class="tinymce" name="body">
+                              <?php echo $postresult['body']; ?>
+                          </textarea>
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>
+                          <label>Tags</label>
+                      </td>
+                      <td>
+                          <input type="text" name="tags" value = "<?php echo $postresult['tags']; ?>" class="medium" />
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>
+                          <label>Author</label>
+                      </td>
+                      <td>
+                          <input type="text" name="author" value = "<?php echo $postresult['author']; ?>" class="medium" />
+                      </td>
+                  </tr>
+      <tr>
+                      <td></td>
+                      <td>
+                          <input type="submit" name="submit" Value="Save" />
+                      </td>
+                  </tr>
+              </table>
+              </form>
+  <?php } }?>
+          </div>
+      </div>
+  </div>
 
         <!-- Load TinyMCE -->
     <script src="js/tiny-mce/jquery.tinymce.js" type="text/javascript"></script>
