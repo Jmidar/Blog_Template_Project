700cb800f4f59259fb16130f80385c4b1987814c
Arnab Das <arnab.das682@gmail.com>
2018-03-12 19:06:56 +0600

    view page

--- admin/inbox.php
+++ admin/inbox.php
@@ -9,55 +9,70 @@
 					<thead>
 						<tr>
 							<th>Serial No.</th>
+							<th>Name</th>
+							<th>Email</th>
 							<th>Message</th>
+							<th>Date</th>
+							<th>Action</th>
+						</tr>
+					</thead>
+					<tbody>
+				<?php
+					$query = "SELECT * FROM tbl_contact WHERE status='0' order by id desc";
+					$selected_row = $db->SELECT($query);
+					 if ($selected_row) {
+					 	$i=0;
+					 	while ($result = $selected_row->fetch_assoc()) {
+					 			$i++;
+				?>	
+					 
+						<tr class="odd gradeX">
+							<td><?php echo $i; ?></td>
+							<th><?php echo $result['firstname'].' '.$result['lastname']; ?></td>
+							<td><?php echo $result['email']; ?></td>
+							<td><?php echo $fm->textShorten($result['body'], 30); ?></td>
+							<td><?php echo $fm->formatDate($result['date']); ?></td>
+							<td><a href="viewmsg.php?viewid=<?php echo $result['id']; ?>">View</a> 
+								|| <a href="replymsg.php?replyid=<?php echo $result['id']; ?>">Reply</a>
+								|| <a href="?seenid=<?php echo $result['id']; ?>">Seen</a>
+							</td>
+						</tr>
+				<?php } } ?>
+					</tbody>
+				</table>
+               </div>
+            </div>
+
+
+        	<div class="box round first grid">
+                <h2>Seen Massage</h2>
+                <div class="block">        
+                    <table class="data display datatable" id="example">
+					<thead>
+						<tr>
+							<th>Serial No.</th>
+							<th>Name</th>
+							<th>Email</th>
+							<th>Message</th>
+							<th>Date</th>
 							<th>Action</th>
 						</tr>
 					</thead>
 					<tbody>
 						<tr class="odd gradeX">
 							<td>01</td>
-							<td>Internet</td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-						<tr class="even gradeC">
-							<td>02</td>
-							<td>Explorer </td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-						<tr class="odd gradeX">
-							<td>03</td>
-							<td>Internet</td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-						<tr class="even gradeC">
-							<td>04</td>
-							<td>Explorer </td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-							<tr class="odd gradeX">
-							<td>05</td>
-							<td>Internet</td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-						<tr class="even gradeC">
-							<td>06</td>
-							<td>Explorer </td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-						<tr class="odd gradeX">
-							<td>07</td>
-							<td>Internet</td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-						<tr class="even gradeC">
-							<td>08</td>
-							<td>Explorer </td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
+							<td>Name</td>
+							<td>Email</td>
+							<td>Message</td>
+							<td>Date</td>
+							<td><a href="">Delete</a> 
+							</td>
 						</tr>
 					</tbody>
 				</table>
                </div>
             </div>
+
         </div>
 
         <script type="text/javascript">
