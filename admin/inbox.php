94c831e92bc067baeba78869d81b22b9826b2281
Arnab Das <arnab.das682@gmail.com>
2018-03-13 18:42:03 +0600

    notification

--- admin/inbox.php
+++ admin/inbox.php
@@ -12,6 +12,30 @@
                    }
 }
 ?>
+<?php
+	if (isset($_GET['unseenid'])) {
+		$unseenid = $_GET['unseenid'];
+			$query = "UPDATE tbl_contact SET status = '0' WHERE id = '$unseenid'";
+			$updated_row = $db->update($query);
+			   if ($updated_row) {
+			   		echo "<span class='success'>uneen</span>";
+              }else {
+                   echo "<span class='error'>not unseen !</span>";
+                   }
+}
+?>
+<?php
+	if (isset($_GET['delid'])) {
+		$seenid = $_GET['delid'];
+			$query = "Delete from tbl_contact WHERE id = '$seenid'";
+			$updated_row = $db->update($query);
+			   if ($updated_row) {
+			   		echo "<span class='success'>delete</span>";
+              }else {
+                   echo "<span class='error'>not delete !</span>";
+                   }
+}
+?>
 
         <div class="grid_10">
             <div class="box round first grid">
@@ -79,6 +103,7 @@
 					 	while ($result = $selected_row->fetch_assoc()) {
 					 			$i++;
 				?>	
+
 					 
 						<tr class="odd gradeX">
 							<td><?php echo $i; ?></td>
@@ -86,8 +111,9 @@
 							<td><?php echo $result['email']; ?></td>
 							<td><?php echo $fm->textShorten($result['body'], 30); ?></td>
 							<td><?php echo $fm->formatDate($result['date']); ?></td>
-							<td><a onclick="return confirm('Are you sure for Delete...!!!')" href="viewmsg.php?viewid=<?php echo $result['id']; ?>">Delete</a> 
-								
+							<td><a href="viewmsg.php?viewid=<?php echo $result['id']; ?>">View</a> 
+								|| <a onclick="return confirm('Are you sure for Delete...!!!')" href="?delid=<?php echo $result['id']; ?>">Delete</a> 
+								|| <a onclick="return confirm('Are you sure for send Seen Message BOX...!!!')" href="?unseenid=<?php echo $result['id']; ?>">Unseen</a>
 							</td>
 						</tr>
 				<?php } } ?>
