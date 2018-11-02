bd02fbece4c6c72e2b6caa7f9ad2f94c1f5da7df
Arnab Das <arnab.das682@gmail.com>
2018-11-03 01:36:09 +0600

    custom theme

---
+++ scripts/css.php
@@ -0,0 +1,21 @@
+	<link rel="stylesheet" href="font-awesome-4.5.0/css/font-awesome.css">	
+	<link rel="stylesheet" href="css/nivo-slider.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="lol.css">
+
+<style>
+
+	/*default*/
+	.headersection { background: #ca932f none repeat scroll 0 0; }
+	.icon a { background: #a56e0a none repeat scroll 0 0; color: #fff; }
+	
+
+	/*Green*/
+	.headersection { background: #007000 none repeat scroll 0 0; }
+	.icon a { background: #115d11 none repeat scroll 0 0; color: #e4a83a; }
+	.searchbtn input[type="text"] { solid #1e6d1c; background: #388a2f; }
+	input[type="submit"] {
+    background: #44923f none repeat scroll 0 0; border: 1px solid #0e421b; }
+
+    input {  background-color: #44923f; }
+</style>
